fa22caba720fc5489c62bfc2a7437162b3157543
Kuldeep Nageshwar <coolkn17@gmail.com>
2022-04-22 14:01:47 +0530

    feat: News feed xml parse

--- src/components/HomePage/NewsFeed/NewsFeed.js
+++ src/components/HomePage/NewsFeed/NewsFeed.js
@@ -1,19 +1,34 @@
-import React, { memo } from 'react';
-import { H1 } from '@components/_shared/text';
-
-import './NewsFeed.scss';
+import React, { memo, useState, useEffect } from "react";
+import { H1 } from "@components/_shared/text";
+import { parseString } from "xml2js";
+import axios from "axios";
+import "./NewsFeed.scss";
 
 const NewsFeed = () => {
-	return (
-		<div className='news-feed-wrapper'>
-			<div className='news-feed-container'>
-				<H1 className='title' bold>News</H1>
-				<div className='news-feed-greed'>
-					Coming soon!
-				</div>
-			</div>
-		</div>
-	);
+  const [news, SetNews] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://www.gra.world/category/retail-news/feed/")
+      .then((response) => response.data)
+      .then(async (data) => {
+        parseString(data, { mergeAttrs: true }, (err, result) => {
+          if (err) {
+            throw err;
+          }
+          console.log(result.rss.channel[0].item);
+        });
+      });
+  }, []);
+  return (
+    <div className="news-feed-wrapper">
+      <div className="news-feed-container">
+        <H1 className="title" bold>
+          News
+        </H1>
+        <div className="news-feed-greed">Coming soon!</div>
+      </div>
+    </div>
+  );
 };
 
 export default memo(NewsFeed);
