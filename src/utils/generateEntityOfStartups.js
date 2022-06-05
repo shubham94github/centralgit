76ddba77ad75d01c6b18324d97d6c8d08b19b335
Kuldeep Nageshwar <35416870+kuldeepNageshwar17@users.noreply.github.com>
2022-06-05 09:30:21 +0530

    feat : View all with a-z Sort chaged RH104

--- src/utils/generateEntityOfStartups.js
+++ src/utils/generateEntityOfStartups.js
@@ -1,27 +1,29 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
-export const generateEntityOfStartups = (newStartups, ratedStartups, relatedStartups) => (
-	[
-		{
-			id: uuid(),
-			value: 'Top Rated',
-			label: 'Top Rated',
-			startups: ratedStartups,
-			sortData: 'Rated',
-		},
-		{
-			id: uuid(),
-			value: 'New',
-			label: 'New',
-			startups: newStartups,
-			sortData: 'Added',
-		},
-		{
-			id: uuid(),
-			value: 'Related',
-			label: 'Recommended',
-			startups: relatedStartups,
-			sortData: 'Related',
-		},
-	]
-);
+export const generateEntityOfStartups = (
+  newStartups,
+  ratedStartups,
+  relatedStartups
+) => [
+  {
+    id: uuid(),
+    value: "Top Rated",
+    label: "Top Rated",
+    startups: ratedStartups,
+    sortData: "A-Z",
+  },
+  {
+    id: uuid(),
+    value: "New",
+    label: "New",
+    startups: newStartups,
+    sortData: "Added",
+  },
+  {
+    id: uuid(),
+    value: "Related",
+    label: "Recommended",
+    startups: relatedStartups,
+    sortData: "A-Z",
+  },
+];
